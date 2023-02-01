aa885996eabdb5ad64efb98d022a387e20af3cdc
jaf107 <48786629+jaf107@users.noreply.github.com>
2023-02-01 16:01:23 +0600

    Try catch

--- Controllers/AlertController.cs
+++ Controllers/AlertController.cs
@@ -14,44 +14,52 @@ namespace TradingExecutorAPI.Controllers
         [HttpPost("/forwardalert", Name = "ForwardAlert")]
         public async Task<string> ForwardAlert()
         {
-            StreamWriter? sw = null;
-            var req = Request;
-
-            var callerUrl = Request.Headers.Referer;
-            // Read Request Body
-            StreamReader streamReader = new StreamReader(req.Body, Encoding.UTF8);
-            string reqBody = await streamReader.ReadToEndAsync();
-
-            // Validate Token
-            bool valid = new AlertService().ValidateService(req.Headers["CallerToken"], reqBody,callerUrl);
-            if (!valid)
-            {
-                sw = new StreamWriter("AlertErrors.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + "Token Validation Error");
-                return "Token Validation Error";
-            }
-            // Write to File
             try
             {
-                sw = new StreamWriter("ClientAlertDetails.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + reqBody);
-            }
-            catch (IOException ex)
-            {
-                sw = new StreamWriter("AlertErrors.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + ex.Message);
-            }
-            catch (Exception ex)
-            {
-                sw = new StreamWriter("AlertErrors.txt", true);
-                await sw.WriteLineAsync(DateTime.Now + " , " + ex.Message);
-            }
-            finally
-            {
-                await sw!.DisposeAsync();
-            }
+                StreamWriter? sw = null;
+                var req = Request;
 
-            return SuccessCode;
+                var callerUrl = Request.Headers.Referer;
+                // Read Request Body
+                StreamReader streamReader = new StreamReader(req.Body, Encoding.UTF8);
+                string reqBody = await streamReader.ReadToEndAsync();
+
+                // Validate Token
+                bool valid = new AlertService().ValidateService(req.Headers["CallerToken"], reqBody, callerUrl);
+                if (!valid)
+                {
+                    sw = new StreamWriter("AlertErrors.txt", true);
+                    await sw.WriteLineAsync(DateTime.Now + " , " + "Token Validation Error");
+                    return "Token Validation Error";
+                }
+                // Write to File
+                try
+                {
+                    sw = new StreamWriter("ClientAlertDetails.txt", true);
+                    await sw.WriteLineAsync(DateTime.Now + " , " + reqBody);
+                }
+                catch (IOException ex)
+                {
+                    sw = new StreamWriter("AlertErrors.txt", true);
+                    await sw.WriteLineAsync(DateTime.Now + " , " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    sw = new StreamWriter("AlertErrors.txt", true);
+                    await sw.WriteLineAsync(DateTime.Now + " , " + ex.Message);
+                }
+                finally
+                {
+                    await sw!.DisposeAsync();
+                }
+
+                return SuccessCode;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
         }
 
 
